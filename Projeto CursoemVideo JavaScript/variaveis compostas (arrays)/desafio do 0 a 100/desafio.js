5e6c235c62efda0678f518ada14f0eb569e170a9
JOHNOVERDADEIROEUNICO <joaopedroteixeira26@gmail.com>
2025-11-06 11:12:36 -0300

    leve update
    tudo o que fiz

--- Projeto CursoemVideo JavaScript/variaveis compostas (arrays)/desafio do 0 a 100/desafio.js
+++ Projeto CursoemVideo JavaScript/variaveis compostas (arrays)/desafio do 0 a 100/desafio.js
@@ -2,10 +2,14 @@ function Contar(){
     let num = document.getElementById('bNumber');
     let tab = document.getElementById('seleTab');
     let array = [];
+    let texto = document.getElementById('text')
 
     function Salvar(){
         if(num.value.length == 0 || Number(num.value) < 0 || Number(num.value) > 100){
             window.alert('Por favor, digite um número entre 0 á 100.')
+        }
+        else if(array.indexOf(Number(num.value)) != -1){
+            window.alert(`O número já foi digitado, digite outro por favor.`)
         }
         else{ 
             numNumber = Number(num.value)
@@ -15,7 +19,11 @@ function Contar(){
             item.text = `O número ${numNumber} foi gerado e salvo com sucesso`;
             
             tab.appendChild(item);
+            texto.innerHTML = ' '
         }
+
+        num.value = ' ';
+        num.focus();
     }
     function Show(){
         if(array.length == 0){
@@ -28,7 +36,6 @@ function Contar(){
 
             const media = sum/array.length;
 
-            let texto = document.getElementById('text')
             texto.innerHTML = `<p>Os valores digitados foram: ${array}</p>`
             
             texto.innerHTML += `<p>A soma de todos os valore é: ${sum}</p>`
@@ -44,6 +51,3 @@ function Contar(){
 }
 
 const s = Contar();
-
-
-
